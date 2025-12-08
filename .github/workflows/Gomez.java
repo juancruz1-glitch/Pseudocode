bac566a059bc86caaf1494c9d709be0da6e11fe6
juancruz1-glitch <juan.cruz1@csuglobal.edu>
2025-12-08 13:55:02 -0500

    Add files via upload

---
+++ .github/workflows/Gomez.java
@@ -0,0 +1,10 @@
+
+public class Gomez {
+public static void main(String args[])
+{ //ALL CODE GOES HERE 
+	
+	System.out.println("Gomez!!");
+	
+	System.out.println("5537 Ridgeton Hill Cout" + "fairfax, Va 22032 "); 
+}
+}
